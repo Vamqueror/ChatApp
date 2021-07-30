6ed2f27e0c46e0e355dfceadac97f524b5208c8c
Vamqueror <liorx444@gmail.com>
2021-07-30 14:22:32 +0300

    group members displayed on hover

--- chat/src/components/GroupList.tsx
+++ chat/src/components/GroupList.tsx
@@ -1,7 +1,8 @@
 import "../App.css";
-import { ListGroup, ListGroupItem } from "react-bootstrap";
+import { ListGroup, ListGroupItem,OverlayTrigger,Popover } from "react-bootstrap";
 import { useGroup, useCurrentGroupUpdate } from "../Context/GroupProvider";
 import { useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const GroupList = () => {
   const groups = useGroup();
@@ -10,9 +11,21 @@ const GroupList = () => {
   const renderGroups = (): JSX.Element[] => {
     return groups.map((element, index) => {
       return (
-        <ListGroupItem action key={index} eventKey={element.id}>
+        <OverlayTrigger
+      trigger="hover"
+      placement="left"
+      overlay={
+        <Popover id={element.id}>
+          <Popover.Title as="h3">{`Group Members`}</Popover.Title>
+          <Popover.Content>
+            {element.members.map((member)=>`${member}\n`)}
+          </Popover.Content>
+        </Popover>
+      }
+    ><ListGroupItem action key={index} eventKey={element.id}>
           {element.name}
         </ListGroupItem>
+    </OverlayTrigger>
       );
     });
   };
